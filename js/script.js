8fa69207f0f48628c7042e3fd40f6dddc0a3d9c9
MitsukiSuganami <57669497+MitsukiSuganami@users.noreply.github.com>
2022-08-22 10:30:20 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,88 +1,56 @@
-$(function () {
-    startScanner();
+Quagga.init({
+    inputStream: {
+        name: 'Live',
+        type: 'LiveStream',
+        target: document.querySelector('#interactive'), //埋め込んだdivのID
+        constraints: {
+            facingMode: 'environment',
+            frameRate: 60
+        },
+
+        area: { //必要ならバーコードの読み取り範囲を調整できる（この場合は、上30%/下30%は読み取りしない）
+            top: "30%",
+            right: "0%",
+            left: "0%",
+        bottom: "30%" }
+    },
+
+    locator: {
+        patchSize: 'medium',
+        halfSample: true
+    },
+
+    numOfWorkers: 2,
+    decoder: {
+        readers: ['ean_reader'] //ISBNは基本的にこれ（他にも種類あり）
+    },
+    locate: true
+},
+err => {
+    if (!err) {
+        Quagga.start();
+        // alert("started");
+    }
 });
 
-const startScanner = () => {
-    Quagga.init({
-        inputStream: {
-            name: "Live",
-            type: "LiveStream",
-            target: document.querySelector('#photo-area'),
-            constraints: {
-                decodeBarCodeRate: 3,
-                successTimeout: 500,
-                codeRepetition: true,
-                tryVertical: true,
-                frameRate: 60,
-                width: 640,
-                height: 480,
-                facingMode: "environment"
-            },
-        },
-        decoder: {
-            readers: [
-                "ean_reader"
-            ]
-        },
+Quagga.onDetected(success => {
+    const code = success.codeResult.code;
+    if (calc(code)) alert(code);
+});
 
-    }, function (err) {
-        if (err) {
-            console.log(err);
-            return
-        }
+const calc = isbn => {
+    const arrIsbn = isbn.
+    toString().
+    split("").
+    map(num => parseInt(num));
+    let remainder = 0;
+    const checkDigit = arrIsbn.pop();
 
-        console.log("Initialization finished. Ready to start");
-        Quagga.start();
-
-        // Set flag to is running
-        _scannerIsRunning = true;
+    arrIsbn.forEach((num, index) => {
+         remainder += num * (index % 2 === 0 ? 1 : 3);
     });
+    remainder %= 10;
+    remainder = remainder === 0 ? 0 : 10 - remainder;
 
-    Quagga.onProcessed(function (result) {
-        var drawingCtx = Quagga.canvas.ctx.overlay,
-            drawingCanvas = Quagga.canvas.dom.overlay;
-
-        if (result) {
-            if (result.boxes) {
-                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                result.boxes.filter(function (box) {
-                    return box !== result.box;
-                }).forEach(function (box) {
-                    Quagga.ImageDebug.drawPath(box, {
-                        x: 0,
-                        y: 1
-                    }, drawingCtx, {
-                        color: "green",
-                        lineWidth: 2
-                    });
-                });
-            }
-
-            if (result.box) {
-                Quagga.ImageDebug.drawPath(result.box, {
-                    x: 0,
-                    y: 1
-                }, drawingCtx, {
-                    color: "#00F",
-                    lineWidth: 2
-                });
-            }
-
-            if (result.codeResult && result.codeResult.code) {
-                Quagga.ImageDebug.drawPath(result.line, {
-                    x: 'x',
-                    y: 'y'
-                }, drawingCtx, {
-                    color: 'red',
-                    lineWidth: 3
-                });
-            }
-        }
-    });
-
-    //barcode read call back
-    Quagga.onDetected(function (result) {
-        console.log(result.codeResult.code);
-        alert("codeResult=" + codeResult);
-    });
-}
+    return checkDigit === remainder;
+};

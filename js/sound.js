2c9d26916fff4c8e3aece3abb42655db5bbf9523
MitsukiSuganami <57669497+MitsukiSuganami@users.noreply.github.com>
2022-08-22 23:17:36 +0900

    Add files via upload

--- js/sound.js
+++ js/sound.js
@@ -131,17 +131,46 @@ function successProgram () {
     var statusMessage = document.getElementById("statusMessage");
     statusMessage.innerHTML = "読み取り成功";
 
-    // ビジーwaitを使う方法
-    function sleep(waitMsec) {
-        var startMsec = new Date();
+    // // ビジーwaitを使う方法
+    // function sleep(waitMsec) {
+    //     var startMsec = new Date();
    
-        // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
-        while (new Date() - startMsec < waitMsec);
-    }
+    //     // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
+    //     while (new Date() - startMsec < waitMsec);
+    // }
    
-    sleep(3000);
+    // sleep(3000);
 
-    statusMessage.innerHTML = "読取できます";
+    // setIntervalを使う方法
+    function sleep(waitSec, callbackFunc) {
+    
+        // 経過時間（秒）
+        var spanedSec = 0;
+    
+        // 1秒間隔で無名関数を実行
+        var id = setInterval(function () {
+    
+            spanedSec++;
+    
+            // 経過時間 >= 待機時間の場合、待機終了。
+            if (spanedSec >= waitSec) {
+    
+                // タイマー停止
+                clearInterval(id);
+    
+                // 完了時、コールバック関数を実行
+                if (callbackFunc) callbackFunc();
+            }
+        }, 1000);
+    
+    }
+    
+    sleep(3, function () {
+        statusMessage.innerHTML = "読取できます";
+        // console.log('5秒経過しました！');
+    });
+
+    // statusMessage.innerHTML = "読取できます";
 }
 
 function notSuccessProgram () {

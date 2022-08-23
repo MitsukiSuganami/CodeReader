068cdc77b34165a0ef23980979f9b707c306428e
MitsukiSuganami <57669497+MitsukiSuganami@users.noreply.github.com>
2022-08-23 17:11:08 +0900

    Add files via upload

--- js/sound.js
+++ js/sound.js
@@ -196,32 +196,36 @@ function notSuccessProgram () {
    
     // sleep(3000);
 
-    // setIntervalを使う方法
-    function sleep(waitSec, callbackFunc) {
+    // // setIntervalを使う方法
+    // function sleep(waitSec, callbackFunc) {
     
-        // 経過時間（秒）
-        var spanedSec = 0;
+    //     // 経過時間（秒）
+    //     var spanedSec = 0;
     
-        // 1秒間隔で無名関数を実行
-        var id = setInterval(function () {
+    //     // 1秒間隔で無名関数を実行
+    //     var id = setInterval(function () {
     
-            spanedSec++;
+    //         spanedSec++;
     
-            // 経過時間 >= 待機時間の場合、待機終了。
-            if (spanedSec >= waitSec) {
+    //         // 経過時間 >= 待機時間の場合、待機終了。
+    //         if (spanedSec >= waitSec) {
     
-                // タイマー停止
-                clearInterval(id);
+    //             // タイマー停止
+    //             clearInterval(id);
     
-                // 完了時、コールバック関数を実行
-                if (callbackFunc) callbackFunc();
-            }
-        }, 1000);
+    //             // 完了時、コールバック関数を実行
+    //             if (callbackFunc) callbackFunc();
+    //         }
+    //     }, 1000);
     
-    }
+    // }
     
-    sleep(3, function () {
-        statusMessage.innerHTML = "スリープ解除";
-        // console.log('5秒経過しました！');
+    // sleep(3, function () {
+    //     statusMessage.innerHTML = "スリープ解除";
+    //     // console.log('5秒経過しました！');
+    // });
+
+    $(document).ready(function(){
+        setTimeout(function(){ statusMessage.innerHTML = "スリープ解除"; }, 3000);
     });
 }

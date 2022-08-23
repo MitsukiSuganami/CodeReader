ed39baa0ddcbbe5ed758e277dc6cd0b0c90be81d
MitsukiSuganami <57669497+MitsukiSuganami@users.noreply.github.com>
2022-08-23 10:56:37 +0900

    Add files via upload

--- js/audioTest.js
+++ js/audioTest.js
@@ -2,3 +2,8 @@ document.getElementById("audioPlay").onclick = function(){
     audioElement.currentTime = 0;
     document.getElementById("audioElement").play();
 };
+
+function playAudio () {
+    audioElement.currentTime = 0;
+    document.getElementById("audioElement").play();
+}
